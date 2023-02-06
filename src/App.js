cb807136b2c433bc07382f4c5709a99ac71cb28b
cc-aeya <aaminah.x@icloud.com>
2023-02-06 18:56:25 +0000

    creates components and imported react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-
 import './App.css';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Project from './pages/Project';
+import PGallery from './components/PGallery';
+import Contact from './pages/Contact';
+
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     < >
+    <Home />
+
+    <Header />
+
+    <Project />
+
+    <PGallery />
+
+    <Contact />
     </>
   );
 }
