6fde82b07c8d95137a3a27c4db8233fdfcc2979d
cc-aeya <aaminah.x@icloud.com>
2023-02-24 15:13:02 +0000

    re-structured contact page and styled it

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,23 +1,42 @@
-// import Header from "../components/Header";
-
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Contact(){
-return(
-    <>
-    <h1>Let's Get in Touch</h1>
-    <form>
-        <p>Name</p>
-        <p>Email</p>
-        <p>Message</p>
-        <button>Submit</button>
-    </form>
 
-    <a href="#">LinkedIn</a>
-    <a href="#">GitHub</a>
-    <a href="#">CV PDF</a>
+    const handleSubmit = event => {
+        event.preventDefault()
+    }
+    return (
+        <>
+        <div className="contactBg div-wrapper">
 
-    </>
-)
+            <div className="background-div">
+                <h2 className="form-h2">Let's Get in Touch</h2>
+
+                <form onSubmit={handleSubmit} >
+                    <input type="text" placeholder="Name" />
+                    <input  placeholder="Email Address" />
+                    <textarea placeholder="Enter Your Message" cols="30" rows="10" ></textarea>
+                    <button className="f-btn" >Submit</button>
+                </form>
+
+                <div className="icon-div"> FB
+                    <a href="https://www.facebook.com/">
+                        {/* <FontAwesomeIcon className="icon fb" icon="fa-brands fa-facebook-f" size="lg"/> */}
+                    </a> IG
+                    <a href="https://instagram.com/"> 
+                        {/* <FontAwesomeIcon className="icon ig" icon="fa-brands fa-instagram" size="lg" /> */}
+                    </a> TW
+                    <a href="https://twitter.com/">
+                        {/* <FontAwesomeIcon className="icon tw" icon="fa-brands fa-twitter" size="lg"/> */}
+                    </a> In
+                    <a href="https://linkedin.com/"> 
+                        {/* <FontAwesomeIcon className="icon in" icon="fa-brands fa-linkedin-in" size="lg"/> */}
+                    </a> 
+                </div>
+            </div>
+        </div>
+        </>
+    )
 }
 
-export default Contact;
+export default Contact
