d37749c750179a68d1481717deca893dff401739
cc-aeya <aaminah.x@icloud.com>
2023-03-03 17:20:50 +0000

    changing contact page layout

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,22 +8,40 @@ function Contact(){
     }
     return (
         <>
-        <div className="contactBg div-wrapper">
+        <main className="contactBg main-wrapper">
+            <h1 className="contact-title">Let's Get in Touch</h1>
+            <section className="contact-grid">
 
-            <div className="background-div">
-                <h2 className="form-h2">Let's Get in Touch</h2>
+                <aside className="contact-info"> 
+                    {/* <h3>contact Information</h3> */}
+                   <div className="text" >
+                        <h3>Name</h3>
+                        <h4>Ayah Sheikh</h4>
+                    </div> 
+                    <div className="text">
+                        <h3>Email</h3>
+                        <h4>ayahshaykgmail.com</h4>
+                    </div>
+                    <div className="text">
+                        <h3>Location</h3>
+                        <h4>London, UK</h4>
+                    </div>
+                </aside>
+           
+                <div className="background-div" id="contact">
 
-                <form onSubmit={handleSubmit} >
-                    <input type="text" placeholder="Name" />
-                    <input  placeholder="Email Address" />
-                    <textarea placeholder="Enter Your Message" cols="30" rows="10" ></textarea>
-                    <button className="f-btn" >Submit</button>
-                </form>
-            </div>
+                    <form onSubmit={handleSubmit} >
+                        <input type="text" placeholder="Name" />
+                        <input  placeholder="Email Address" />
+                        <textarea placeholder="Enter Your Message" cols="30" rows="10" ></textarea>
+                        <button className="f-btn" >Submit</button>
+                    </form>
+                </div>
+            </section>
 
-                <div className="icon-div"> 
-                    <a href="https://github.com/ayahxsheikh">
-                    <FontAwesomeIcon icon="fab fa-github" className="icon gh" size="lg"/>
+            <section className="icon-div">
+                <a href="https://github.com/ayahxsheikh">
+                        <FontAwesomeIcon icon="fab fa-github" className="icon gh" size="lg"/>
                     </a> 
                     <a href="https://instagram.com/"> 
                         <FontAwesomeIcon  icon="fa-brands fa-instagram" className="icon ig" size="lg" />
@@ -34,8 +52,9 @@ function Contact(){
                     <a href="https://linkedin.com/"> 
                         <FontAwesomeIcon icon="fa-brands fa-linkedin-in" className="icon in" size="lg"/>
                     </a> 
-                </div>
-        </div>
+            </section>
+
+        </main>
         </>
     )
 }
