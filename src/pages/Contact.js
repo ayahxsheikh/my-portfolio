5a2fb4af73b5539832a8ba7ab134c53d176fa92d
cc-aeya <aaminah.x@icloud.com>
2023-03-07 18:37:26 +0000

    chnaged color-scheme contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,15 +9,15 @@ const styles = {
     p: {
         color: '#222121da'
     },
-    text1: {
-        backgroundColor: '#deeff2ee'
-    },
-    text2: {
-        backgroundColor:'#f1e6f1ee'
-    },
-    text3: {
-        backgroundColor: '#eef6dbee'
-    }
+    // text1: {
+    //     backgroundColor: '#deeff2ee'
+    // },
+    // text2: {
+    //     backgroundColor:'#f1e6f1ee'
+    // },
+    // text3: {
+    //     backgroundColor: '#eef6dbee'
+    // }
 }
 
 function Contact(){
@@ -34,31 +34,30 @@ function Contact(){
 
                 <aside className="contact-info"> 
 
-                   <div className="contact-text" style={styles.text1} >
+                   <div className="contact-text text1" style={styles.text1} >
                         <FontAwesomeIcon icon="fas fa-user" className="icon"/>
                         <div className="text-info">
-                            <h3 style={styles.p}>Name:</h3>
-                            <h4>Ayah Sheikh</h4>
+                            <h4 style={styles.p}>Name:</h4>
+                            <h4>A Sheikh</h4>
                         </div>
                     </div> 
 
-                    <div className="contact-text" style={styles.text2}>
+                    <div className="contact-text text2" style={styles.text2}>
                         <FontAwesomeIcon icon="fas fa-envelope" className="icon"/>
                         <div className="text-info">
-                            <h3 style={styles.p}>Email:</h3>
-                            <h4>ayahshayk@gmail.com</h4>
+                            <h4 style={styles.p}>Email:</h4>
+                            <h4>aaminah.xa@gmail.com</h4>
                         </div>
                     </div>
 
-                    <div className="contact-text" style={styles.text3} >
+                    <div className="contact-text text3" style={styles.text3} >
                         <FontAwesomeIcon icon="fas fa-map-marked-alt" className="icon"/>
                         <div className="text-info">
-                            <h3 style={styles.p}>Location:</h3>
+                            <h4 style={styles.p}>Location:</h4>
                             <h4>London, UK</h4>
                         </div>
                     </div>
                 </aside>
-                {/* <h3>Fill out the form</h3> */}
            
                 <div className="background-div" id="contact">
                     <form onSubmit={handleSubmit} >
@@ -70,6 +69,7 @@ function Contact(){
                 </div>
             </section>
 
+                <h3 style={styles.h3}>Connect with me on Socials</h3>
             <section className="icon-div">
                 <a href="https://github.com/ayahxsheikh">
                         <FontAwesomeIcon icon="fab fa-github" className="icon gh" size="lg"/>
