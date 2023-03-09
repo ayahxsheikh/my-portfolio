9e24dbcf50b7ae407726014c9554ce6f85c1bb04
cc-aeya <aaminah.x@icloud.com>
2023-03-09 14:43:43 +0000

    added figure/figcaption for img overlay, needs css

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,13 @@ function Project(props){
     return (
         <>
             <div className="project-card">
-                <img className="proj-img" src={props.image} alt="title"/>
+                <figure className="img-overlay">
+                    <img className="proj-img" src={props.image} alt="title"/>
+                    <figcaption>
+                        <p>{props.title}</p>
+                        <p>{}</p>
+                    </figcaption>
+                </figure>
                 {/* <div className="card-btm">
                     <h4 className="p-title">{props.title}</h4>
                 <button className="btn">View More</button>
