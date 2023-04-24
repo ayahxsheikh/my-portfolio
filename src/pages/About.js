991c06d244a98c4c86e811427de869d4b80f4ddc
cc-aeya <aaminah.x@icloud.com>
2023-04-24 19:17:51 +0100

    fixed centering 1st skills row with justify-content

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,24 +17,62 @@ function About(){
       
       <main className="aboutBg aboutWrapper">
 
-        <section className="my-info">
+        <section className="about-section">
         <h3> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe veritatis perspiciatis quibusdam eius inventore numquam fugit. Obcaecati earum at ipsam cupiditate, debitis, doloremque, facere dolores molestiae omnis eius laudantium! Amet. </h3>
         </section>
 
-        <section className="tech-skills">
-            <h3 className='techH3'>Technologies I work with:</h3>
-            <div className="tech-logos">
-                <img className="html" src={htmlLogo} alt="html-logo" />
-                <img className="css" src={cssLogo} alt="css-logo" />
-                <img className="js" src={jsLogo} alt="js-logo" />
-                <img className="bootstrap" src={btstrapLogo} alt="bootstrap-logo" />
-                <img className="jquery" src={jqueryLogo} alt="jq-logo" />
-                <img className="vs" src={vsLogo} alt="vs-logo" />
-                <img className="git" src={gitLogo} alt="git-logo" />
-                <img className="node" src={nodejsLogo} alt="node-logo" />
-                <img className="npm" src={npmLogo} alt="npm-logo" />
-                <img className="github" src={githubLogo} alt="github-logo" />
-                <img className="react" src={reactLogo} alt="react-logo" />
+        <section className="skills-section">
+            <h2 className="tools-title">Technologies and Tools</h2>
+            <div className="techLogos1">
+                <figure>
+                    <img className="html" src={htmlLogo} alt="html-logo" />
+                    <figcaption>HTML</figcaption>
+                </figure>
+                <figure>
+                    <img className="css" src={cssLogo} alt="css-logo" />
+                    <figcaption>CSS</figcaption>
+                </figure>
+                <figure>
+                    <img className="js" src={jsLogo} alt="js-logo" />
+                    <figcaption>JavaScript</figcaption>
+                </figure>
+                <figure>
+                    <img className="jquery" src={jqueryLogo} alt="jq-logo" />
+                    <figcaption>jQeury</figcaption>
+                </figure>
+                <figure>
+                        <img className="react" src={reactLogo} alt="react-logo" />
+                        <figcaption>React</figcaption>
+                    </figure>
+              
+
+                <div className="techLogos2">
+                    <figure>
+                        <img className="vs" src={vsLogo} alt="vs-logo" />
+                        <figcaption>VS Code</figcaption>
+                    </figure>
+                    <figure>
+                        <img className="git" src={gitLogo} alt="git-logo" />
+                        <figcaption>GIT</figcaption>
+                    </figure>
+                    <figure>
+                        <img className="node" src={nodejsLogo} alt="node-logo" />
+                        <figcaption>NodeJS</figcaption>
+                    </figure>
+                    <figure>
+                        <img className="npm" src={npmLogo} alt="npm-logo" />
+                        <figcaption>NPM</figcaption>
+                    </figure>
+                    <figure>
+                        <img className="github" src={githubLogo} alt="github-logo" />
+                        <figcaption>GitHub</figcaption>
+                    </figure>
+                  
+                {/* <figure>
+                    <img className="bootstrap" src={btstrapLogo} alt="bootstrap-logo" />
+                    <figcaption>Bootstrap</figcaption>
+                </figure> */}
+                </div>
 
             </div>
         </section>
