5bd3b53989e859df34bba8a88212367f45769257
cc-aeya <aaminah.x@icloud.com>
2023-04-24 15:31:13 +0100

    adds logos to assets and html css ones to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,24 @@
 import '../styles/about.css'
+import cssLogo from '../assets/cssLogo.jpg'
+import htmlLogo from '../assets/htmlLogo.jpg'
+
 
 function About(){
     return (
       
-      <main className="aboutBg">
+      <main className="aboutBg aboutWrapper">
 
         <section className="my-info">
-        <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe veritatis perspiciatis quibusdam eius inventore numquam fugit. Obcaecati earum at ipsam cupiditate, debitis, doloremque, facere dolores molestiae omnis eius laudantium! Amet. </p>
+        <h3> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe veritatis perspiciatis quibusdam eius inventore numquam fugit. Obcaecati earum at ipsam cupiditate, debitis, doloremque, facere dolores molestiae omnis eius laudantium! Amet. </h3>
         </section>
 
-        <section className="skills">
-            <h3>Technologies I work with:</h3>
+        <section className="tech-skills">
+            <h3 className='techH3'>Technologies I work with:</h3>
+            <div className="tech-logos">
+                <img className="html" src={htmlLogo} alt="html-logo" />
+                <img className="css" src={cssLogo} alt="css-logo" />
+
+            </div>
         </section>
       </main>
       
