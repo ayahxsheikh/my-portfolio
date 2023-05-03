e9e8c89bbff01e00f98c191471975526de2aec1b
cc-aeya <aaminah.x@icloud.com>
2023-05-03 14:19:59 +0100

    removed black bg from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,12 @@ function Footer(){
     return (
         <footer className="footer">
             <p className="freepik">
-               &copy;A Sheikh. 2023 - All Rights Reserved 
+               &copy; A Sheikh. 2023 - All Rights Reserved 
             </p>
-                <p className="freepik">
+                {/* <p className="freepik">
                     <a href="https://www.freepik.com/free-vector/collection-designer-work_5030709.htm#&position=0&from_view=undefined"></a> 
                     Image by pikisuperstar on Freepik
-                </p>
+                </p> */}
         </footer>
     )
 }
