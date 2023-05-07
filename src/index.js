95d0c2e371ae11cdd93c65970c75a22de0d8cf6f
cc-aeya <aaminah.x@icloud.com>
2023-05-07 15:39:46 +0100

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import { HashBrowser } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashBrowser>
+    <HashRouter>
     <App />
-    </HashBrowser>
+    </HashRouter>
   </React.StrictMode>
 );
 
